4c4ba7dae9150de4d04ab7f8415683f0cee98c58
Wang <dihwang@ebay.com>
2016-08-11 13:21:45 +0800

    begining

--- examples/basic-browser/lib/main.js
+++ examples/basic-browser/lib/main.js
@@ -1,4 +1,25 @@
-dust.render("views/hello", {"version": dust.version}, function(err, out) {
-  if(err) console.error(err);
-  document.getElementsByTagName('body')[0].innerHTML = out;
+var json = {
+  "languages": [
+    {
+      "name": "HTML",
+      "creators": ["Tim Berners Lee"]
+    },
+    {
+      "name": "CSS",
+      "creators": ["Håkon Wium Lie", "Bert Bos"]
+    },
+    {
+      "name": "JavaScript",
+      "creators": ["Brendan Eich"]
+    },
+    {
+      "name": "Dust",
+      "creators": ["akdubya"]
+    }
+  ]
+};
+dust.render("views/hello", json, function(err, out) {
+	if (err)
+		console.error(err);
+	document.getElementsByTagName('body')[0].innerHTML = out;
 });
